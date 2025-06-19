8b8301e24bc7b88a7e1f14d7f4a6182f9bd08b4c
YayChia <yay.chia9@gmail.com>
2025-06-19 16:35:29 +0900

    fix: AuthProvider path issue

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,9 +8,8 @@ const inter = Inter({ subsets: ['latin'] })
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
-        <AuthProvider>{children}</AuthProvider>
-      </body>
+      <body className={inter.className}>{children}</body>
     </html>
   )
 }
+
