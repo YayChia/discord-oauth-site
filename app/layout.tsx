f82586e9fc114d4a735323e5be4fdb85e07bf16b
YayChia <yay.chia9@gmail.com>
2025-06-19 18:23:55 +0900

    fix: add children type to RootLayout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,13 @@
-import './globals.css';
-
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+import './globals.css'
+import Providers from "./providers";
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
   return (
     <html lang="en">
       <body>{children}</body>
     </html>
-  );
+  )
 }
